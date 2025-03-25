55da701fb6255b592573ce8c0b2e9845d369725c
rakaso598 <nexonaddress98@gmail.com>
2025-03-25 10:25:44 +0900

    bugfix: 경로오류 변경사항 반영

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './styles/global.css';
 import Header from './layout/Header';
 import Home from './pages/home/Home';
-import CreateStudy from './pages/create-study/CreateStudy';
+import CreateStudy from './pages/study-create/CreateStudy';
 import TodayFocus from './pages/today-focus/TodayFocus';
 import TodayHabit from './pages/today-habit/TodayHabit';
 import StudyDetail from './pages/study-detail/StudyDetail';
@@ -15,7 +15,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/create-study' element={<CreateStudy />} />
+          <Route path='/study-create' element={<CreateStudy />} />
           <Route path='/study-detail' element={<StudyDetail />} />
           <Route path='/today-focus' element={<TodayFocus />} />
           <Route path='/today-habit' element={<TodayHabit />} />
