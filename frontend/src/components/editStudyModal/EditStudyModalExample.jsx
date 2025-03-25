41795fe0167b1e9ce6d9a7c4ffad394f8934bdce
rakaso598 <nexonaddress98@gmail.com>
2025-03-25 11:52:29 +0900

    chore: 중복된 코드 제거 & 이 컴포넌트에 대한 설명 추가

--- frontend/src/components/editStudyModal/EditStudyModalExample.jsx
+++ frontend/src/components/editStudyModal/EditStudyModalExample.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import EditStudyModalButton from './EditStudyModalButton';
 
+/**
+ * 스터디 상세 - 비번 모달 (수정하러가기 버튼 존재) 기능을 사용하기 위한 예제 컴포넌트입니다.
+ * 
+ * import EditStudyModalExample from './components/editStudyModal/EditStudyModalExample';
+ * <Route path='/edit-study-modal-example' element={<EditStudyModalExample />} />
+ * 
+ * 라우팅 시 App.jsx에 위 임포트와 라우트패스 코드 기입하면 동작 확인 가능합니다. (라우팅경로 언제든지 변경될수있음)
+ */
 const EditStudyModalExample = () => {
   return (
     <div>
-      {/* 기본 버튼 텍스트를 사용하는 경우 */}
-      <EditStudyModalButton />
-
-      {/* 사용자 정의 버튼 텍스트를 사용하는 경우 */}
       <EditStudyModalButton buttonText="스터디 정보 수정" />
-
-      {/* 다른 버튼들과 함께 사용하는 경우 */}
-      <button>다른 버튼</button>
-      <EditStudyModalButton buttonText="수정 모달 열기" />
     </div>
   );
 };
