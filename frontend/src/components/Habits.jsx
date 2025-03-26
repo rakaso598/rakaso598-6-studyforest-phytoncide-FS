9b2bfadb3b27fb9871d1e32967dc3feab0a5e2b1
choi-mk <xhrlmin10@naver.com>
2025-03-26 11:44:17 +0900

    feat(habitlist):add click style change and add media

--- frontend/src/components/Habits.jsx
+++ frontend/src/components/Habits.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Habits.module.css";
 
 function Habits() {
+  const [habits, setHabits] = useState([
+    { id: 1, title: "미라클 모닝", checked: false },
+    { id: 2, title: "아침 챙겨 먹기", checked: false },
+  ]);
+  const HandleClick = (id) => {
+    setHabits((prevHabits) =>
+      prevHabits.map((habit) =>
+        habit.id === id ? { ...habit, checked: !habit.checked } : habit
+      )
+    );
+  };
   return (
-    <div className={styles["habit-list"]}>
-      <div className={styles["habit-checked"]}>미라클 모닝</div>
-      <div className={styles["habit-unchecked"]}>아침 챙겨 먹기</div>
-      <p className={styles["no-habit-text"]}>
-        아직 습관이 없어요 <br /> 목록 수정을 눌러 습관을 생성해보세요{" "}
-      </p>
+    <div className={styles.habitList}>
+      {habits.length === 0 ? (
+        <p className={styles["noHabitText"]}>
+          아직 습관이 없어요 <br /> 목록 수정을 눌러 습관을 생성해보세요{" "}
+        </p>
+      ) : (
+        habits.map((habit) => (
+          <div id={habit.id} className={styles.habitItem}>
+            <button
+              onClick={() => HandleClick(habit.id)}
+              className={
+                habit.checked ? styles.habitChecked : styles.habitUnchecked
+              }
+            >
+              {habit.title}
+            </button>
+          </div>
+        ))
+      )}
     </div>
   );
 }
--- frontend/src/pages/today-habit/TodayHabit.jsx
+++ frontend/src/pages/today-habit/TodayHabit.jsx
@@ -18,33 +18,31 @@ const TodayHabit = () => {
   }, [currentTime]);
 
   return (
-    <div className={styles["container"]}>
-      <div className={styles["main-container"]}>
-        <div className={styles["top-container"]}>
-          <div className={styles["title-container"]}>
-            <p className={styles["title"]}>title</p>
-            <div className={styles["link-container"]}>
-              <Link to="/today-focus" className={styles["link-text"]}>
+    <div className={styles.container}>
+      <div className={styles.mainContainer}>
+        <div className={styles.topContainer}>
+          <div className={styles.titleContainer}>
+            <p className={styles.title}>title</p>
+            <div className={styles.linkContainer}>
+              <Link to="/today-focus" className={styles.linkText}>
                 오늘의 집중 &gt;
               </Link>
-              <Link to="/" className={styles["link-text"]}>
+              <Link to="/" className={styles.linkText}>
                 홈 &gt;
               </Link>
             </div>
           </div>
-          <div className={styles["time-container"]}>
-            <p className={styles["time-title"]}>현재 시간</p>
-            <div className={styles["time-now"]}>{currentTime}</div>
+          <div className={styles.timeContainer}>
+            <p className={styles.timeTitle}>현재 시간</p>
+            <div className={styles.timeNow}>{currentTime}</div>
           </div>
         </div>
-        <div className={styles["habit-container"]}>
-          <div className={styles["habit-top-container"]}>
-            <p className={styles["habit-title"]}>오늘의 습관</p>
-            <p className={styles["list-text"]}>목록 수정</p>
+        <div className={styles.habitContainer}>
+          <div className={styles.habitTopContainer}>
+            <p className={styles.habitTitle}>오늘의 습관</p>
+            <p className={styles.listText}>목록 수정</p>
           </div>
-          <div>
-            <Habits />
-          </div>
+          <Habits />
         </div>
       </div>
     </div>
