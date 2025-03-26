284354744e719118f0789832d58afff7d8123565
rakaso598 <nexonaddress98@gmail.com>
2025-03-26 15:09:54 +0900

    feat: '스터디상세화면'에 '수정하기' 모달 버튼 추가[프론트엔드]

--- frontend/src/components/editStudyModal/EditStudyModalButton.jsx
+++ frontend/src/components/editStudyModal/EditStudyModalButton.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import EditStudyModal from './EditStudyModal';
 import { useEditStudyModalController } from './EditStudyModalController';
+import './EditStudyModalButton.css';
 
 const EditStudyModalButton = ({ buttonText }) => {
   const { editStudyModal, handleOpenEditStudyModal, handleCloseEditStudyModal } = useEditStudyModalController();
 
   return (
     <>
-      <button onClick={handleOpenEditStudyModal}>{buttonText || '스터디 수정 모달 열기'}</button>
+      <button className='openEditStudyModalButton' onClick={handleOpenEditStudyModal}>{buttonText || '스터디 수정 모달 열기'}</button>
       <EditStudyModal isOpen={editStudyModal} onClose={handleCloseEditStudyModal} />
     </>
   );
