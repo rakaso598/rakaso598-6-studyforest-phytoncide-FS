fa09b2bd768d6a74ecfc48609e43438283813fc3
sebiny <rucy8888@naver.com>
2025-03-26 15:39:44 +0900

    feat: modal동적기능 구현

--- frontend/src/components/TodayHabitCreate.jsx
+++ frontend/src/components/TodayHabitCreate.jsx
@@ -1,44 +1,64 @@
-import React from "react";
-import "./TodayHabitCreate.css";
+import React, { useState } from "react";
+import styles from "./TodayHabitCreate.module.css";
 import trashIcon from "../../public/images/icon/ic_trash.svg";
+import plusIcon from "../../public/images/icon/ic_plus.svg";
 
 const TodayHabitCreate = () => {
+  //habits 설정
+  const [habits, setHabits] = useState([
+    "미라클모닝 6시 기상",
+    "새벽 운동",
+    "저녁 운동",
+  ]);
+  const handleInputChange = (index, updatingHabit) => {
+    const updatedHabits = habits.map((habit, idx) =>
+      idx === index ? updatingHabit : habit
+    );
+    setHabits(updatedHabits);
+  };
+
+  //습관 추가
+  const handleAddHabit = () => {
+    setHabits([...habits, ""]);
+  };
+  //습관 삭제 habits 필터링
+  const handleDeleteHabit = (index) => {
+    const updatedHabits = habits.filter((_, idx) => idx !== index);
+    setHabits(updatedHabits);
+  };
   return (
-    <>
-      <div className="habitCreateModal">
-        <p className="headline">습관 목록</p>
-        <div className="wholeHabitsBox">
-          <div className="singleHabitBoxWithTrashIc">
-            <div className="singleHabitBox">
-              <p className="singleHabit">미라클모닝 6시 기상</p>
-              {/* p가 인풋타입으로 바뀌는듯 */}
+    <div className={styles.habitCreateModal}>
+      <p className={styles.headline}>습관 목록</p>
+      <div className={styles.wholeHabitsBox}>
+        {habits.map((habit, index) => (
+          <div className={styles.singleHabitBoxWithTrashIc} key={index}>
+            <div className={styles.singleHabitBox}>
+              <input
+                type="text"
+                className={styles.singleHabit}
+                value={habit}
+                onChange={(e) => handleInputChange(index, e.target.value)} //map에서 주는 index
+                size={habit.length || 10}
+              />
             </div>
-            <img src={trashIcon} className="trashIcon"></img>
+            <img
+              src={trashIcon}
+              className={styles.trashIcon}
+              onClick={() => handleDeleteHabit(index)}
+              alt="삭제 아이콘"
+            />
           </div>
-          <div className="singleHabitBoxWithTrashIc">
-            <div className="singleHabitBox">
-              <p className="singleHabit">미라클모닝 6시 기상</p>
-              {/* p가 인풋타입으로 바뀌는듯 */}
-            </div>
-            <img src={trashIcon} className="trashIcon"></img>
-          </div>
-          <div className="singleHabitBoxWithTrashIc">
-            <div className="singleHabitBox">
-              <p className="singleHabit">미라클모닝 6시 기상</p>
-              {/* p가 인풋타입으로 바뀌는듯 */}
-            </div>
-            <img src={trashIcon} className="trashIcon"></img>
-          </div>
-
-          <div className="createBox">+</div>
-        </div>
-
-        <div className="Btns">
-          <button className="close">취소</button>
-          <button className="confirmRevision">수정 완료</button>
+        ))}
+        <div className={styles.createBox} onClick={handleAddHabit}>
+          <img src={plusIcon} alt="추가 아이콘" />
         </div>
       </div>
-    </>
+      <div className={styles.btns}>
+        <button className={styles.close}>취소</button>
+        <button className={styles.confirmRevision}>수정 완료</button>
+      </div>
+    </div>
   );
 };
+
 export default TodayHabitCreate;
--- frontend/src/pages/today-habit/TodayHabit.jsx
+++ frontend/src/pages/today-habit/TodayHabit.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import TodayHabitCreate from "../../components/TodayHabitCreate";
+import TodayHabitCreate from "@components/TodayHabitCreate";
 const TodayHabit = () => {
   return (
-    <div>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
       여기는 오늘의습관 입니다.
       <TodayHabitCreate />
     </div>
