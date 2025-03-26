7543a913834426d7c5ad9b3a5ad45bc241683474
Jam1eL1 <23jisu@gmail.com>
2025-03-26 15:33:24 +0900

    feat: StudyDetailNav 추가

--- frontend/src/pages/study-detail/StudyDetailNav.jsx
+++ frontend/src/pages/study-detail/StudyDetailNav.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styles from "./StudyDetailNav.module.css";
+import smileIcon from "/images/icon/ic_smile.svg";
 
 function StudyDetailNav() {
-  return <div>StudyDetailNav</div>;
+  return (
+    <div className={styles.header}>
+      {/* 이부분 나중에 이모지 기능 연동하면 src 변경해야함 */}
+      <div className={styles.emojis}>
+        <button className={styles.emoji}>
+          <img src="/images/stickers/Subtract-1.svg" alt="emoji" />
+        </button>
+        <button className={styles.emoji}>
+          <img src="/images/stickers/Subtract-2.svg" alt="emoji" />
+        </button>
+        <button className={styles.emoji}>
+          <img src="/images/stickers/Subtract-3.svg" alt="emoji" />
+        </button>
+        <button className={styles.emoji}>
+          <img src="/images/stickers/Subtract-4.svg" alt="emoji" />
+        </button>
+        <button className={styles.addBtn}>
+          <img src={smileIcon} />
+          <p>추가</p>
+        </button>
+      </div>
+
+      <nav className={styles.nav}>
+        {/* 이부분 Link 달아서 변경해야함 */}
+        <a href="#">공유하기</a>
+        <span className={styles.divider}>|</span>
+        <a href="#">수정하기</a>
+        <span className={styles.divider}>|</span>
+        <a href="#">스터디 삭제하기</a>
+      </nav>
+    </div>
+  );
 }
 
 export default StudyDetailNav;
