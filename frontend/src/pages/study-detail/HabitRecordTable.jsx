8839e1800270a0c2b3cb177d2cf3bcaff9cc88b1
Jam1eL1 <23jisu@gmail.com>
2025-03-28 00:39:54 +0900

    feat: 요일 셀 습관기록표에 구현

--- frontend/src/pages/study-detail/HabitRecordTable.jsx
+++ frontend/src/pages/study-detail/HabitRecordTable.jsx
@@ -11,11 +11,15 @@ function HabitRecordTable() {
     "건강하게 먹기",
     "1시간 수영",
     "찬물샤워",
+    "찬물샤워",
+    "찬물샤워",
+    "찬물샤워",
   ];
 
   const days = ["월", "화", "수", "목", "금", "토", "일"];
 
   const getRowColor = (rowIndex) => {
+    // 우선 유저가 클릭했다 가정하고 구현
     const colors = [
       "#D2E869",
       "#a1d63f",
@@ -24,12 +28,27 @@ function HabitRecordTable() {
       "#1fcdcd",
       "#C2D94C",
     ];
-    return colors[rowIndex] || "#ffffffca";
+    // 인덱스 벗어나면 회색스티커
+    return colors[rowIndex] || "#818181";
   };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>습관 기록표</h1>
+
+      <div className={styles.gridContainer}>
+        {/* header row */}
+        <div className={styles.headerRow}>
+          <div className={styles.emptyHeaderCell}></div>
+          <div className={styles.daysHeader}>
+            {days.map((day, index) => (
+              <div key={`day-${index}`} className={styles.dayCell}>
+                {day}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
