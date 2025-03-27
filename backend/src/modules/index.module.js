08220e5af5bfd17ba764e8da34242d961af1afec
sebiny <rucy8888@naver.com>
2025-03-27 17:14:57 +0900

    feat: habit create backend

--- backend/src/modules/index.module.js
+++ backend/src/modules/index.module.js
@@ -1,10 +1,11 @@
 import express from "express";
 import healthCheckRouter from "./healthCheck.module.js";
 import editStudyModules from "./editStudyModules/editStudy.module.js";
-
+import habitsRouter from "./habitModules/habits.module.js";
 const router = express.Router();
 
 router.use("/health-check", healthCheckRouter);
 router.use("/api", editStudyModules);
+router.use("/habits", habitsRouter);
 
 export default router;
