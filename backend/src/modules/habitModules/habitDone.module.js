fb74626b8dd6740dfea9d31c6ba1924166b03272
choi-mk <xhrlmin10@naver.com>
2025-03-31 12:44:12 +0900

    feat: habitdone 모듈 수정

---
+++ backend/src/modules/habitModules/habitDone.module.js
@@ -0,0 +1,54 @@
+import express from "express";
+import prisma from "../../db/prisma/client.prisma.js";
+
+const habitDoneRouter = express.Router();
+
+habitDoneRouter.get("/gethabitdone/:habitId/:day", async (req, res, next) => {
+  try {
+    const habitId = Number(req.params.habitId);
+    const day = req.params.day;
+    const habits = await prisma.habitDone.findMany({
+      where: { habitId, dayOfWeek: day },
+    });
+    res.status(201).json(habits);
+  } catch (e) {
+    next(e);
+  }
+});
+
+habitDoneRouter.post("/posthabitdone/:habitId", async (req, res, next) => {
+  try {
+    const habitId = Number(req.params.habitId);
+    const { isDone, dayOfWeek } = req.body;
+    const habits = await prisma.habitDone.create({
+      data: {
+        habitId,
+        isDone,
+        dayOfWeek,
+      },
+    });
+    res.status(201).json(habits);
+  } catch (e) {
+    next(e);
+  }
+});
+
+habitDoneRouter.patch(
+  "/patchhabitdone/:habitDoneId",
+  async (req, res, next) => {
+    try {
+      const data = req.body;
+      const habitDoneId = Number(req.params.habitDoneId);
+      const updatedHabit = await prisma.habitDone.update({
+        where: { id: habitDoneId },
+        data,
+      });
+      if (!updatedHabit) return res.status(404).send("can't find habit");
+      res.status(204).json(updatedHabit);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+export default habitDoneRouter;
--- backend/src/modules/route/habit.module.js
+++ backend/src/modules/route/habit.module.js
@@ -1,8 +1,10 @@
 import express from "express";
 import habitsRouter from "../habitModules/habits.module.js";
+import habitDoneRouter from "../habitModules/habitDone.module.js";
 
 const habitRouter = express.Router();
 
 habitRouter.use("/habits", habitsRouter); // http://localhost:5090/api/habits
+habitRouter.use("/habitdone", habitDoneRouter);
 
 export default habitRouter;
