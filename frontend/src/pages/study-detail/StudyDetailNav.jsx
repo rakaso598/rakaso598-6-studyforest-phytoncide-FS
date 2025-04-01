e87a7b01f6c345c11bb2f95216d796d4c9ed455a
Jam1eL1 <23jisu@gmail.com>
2025-04-01 15:39:07 +0900

    feat: studyDeleteRouter 작성

--- frontend/src/pages/study-detail/StudyDetailNav.jsx
+++ frontend/src/pages/study-detail/StudyDetailNav.jsx
@@ -3,13 +3,12 @@ import styles from "./StudyDetailNav.module.css";
 import smileIcon from "/images/icon/ic_smile.svg";
 import EditStudyModalButton from "@components/edit-study-modal/EditStudyModalButton";
 
-
 function StudyDetailNav() {
   return (
     <div className={styles.header}>
       {/* 이부분 나중에 이모지 기능 연동하면 src 변경해야함 */}
       <div className={styles.emojis}>
-        <StudyEmoji />
+        {/* <StudyEmoji /> */}
         {/* <button className={styles.emoji}>
           <img src="/images/stickers/Subtract-1.svg" alt="emoji" />
         </button>
