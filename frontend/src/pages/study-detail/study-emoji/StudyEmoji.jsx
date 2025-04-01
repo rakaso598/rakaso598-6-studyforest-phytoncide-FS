29adf24b584b52bc485f1945e65cfa1bb7a3d025
rakaso598 <nexonaddress98@gmail.com>
2025-04-01 16:11:06 +0900

    feat: 이모지피커 화면에표시 토글 기능 추가

--- frontend/src/pages/study-detail/study-emoji/StudyEmoji.jsx
+++ frontend/src/pages/study-detail/study-emoji/StudyEmoji.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import styles from "./StudyDetailNav.module.css";
 import smileIcon from "/images/icon/ic_smile.svg";
+import EmojiPicker from 'emoji-picker-react';
 
 function StudyEmoji() {
+  const [showPicker, setShowPicker] = useState(false);
+  const addButtonRef = useRef(null);
+
+  const handleAddButtonClick = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const pickerStyle = {
+    position: 'absolute',
+    top: addButtonRef.current ? addButtonRef.current.offsetHeight + 10 : '50px',
+    left: addButtonRef.current ? addButtonRef.current.offsetLeft : '0',
+    zIndex: 1000,
+  };
+
   return (
-    <div>
-      {/* <button className={styles.emoji}>
+    <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}> {/* marginLeft 추가 */}
+      <button className={styles.emoji}>
         <img src="/images/stickers/Subtract-1.svg" alt="emoji" />
       </button>
       <button className={styles.emoji}>
@@ -16,13 +31,14 @@ function StudyEmoji() {
       </button>
       <button className={styles.emoji}>
         <img src="/images/stickers/Subtract-4.svg" alt="emoji" />
-      </button> */}
-      <button className={styles.addBtn}>
-        <img src={smileIcon} />
+      </button>
+      <button className={styles.addBtn} onClick={handleAddButtonClick} ref={addButtonRef}>
+        <img src={smileIcon} alt="smile" />
         <p>추가</p>
       </button>
+      {showPicker && <div style={pickerStyle}><EmojiPicker /></div>}
     </div>
-  )
+  );
 }
 
-export default StudyEmoji
+export default StudyEmoji;
