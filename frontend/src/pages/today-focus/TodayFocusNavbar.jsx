c7f6fd8402b4810bb3c96500ec8c0702a8b09aa3
Decal <vumublne@naver.com>
2025-04-01 13:07:11 +0900

    fix: 타이머 관련 버그 수정, 그 외 디자인 및 기능 수정

--- frontend/src/pages/today-focus/TodayFocusNavbar.jsx
+++ frontend/src/pages/today-focus/TodayFocusNavbar.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./TodayFocus.module.css";
 
-const TodayFocusNavbar = () => {
+const TodayFocusNavbar = ({ id }) => {
   return (
     <nav className={styles.navBar}>
       <h1 className={styles.navTxt}>연우의 개발공장</h1>
       <div className={styles.navBtnContainer}>
-        <Link to="/today-habit" className={styles.navBtn}>
+        <Link to={`/study/${id}/habit`} className={styles.navBtn}>
           오늘의 습관
           <img src="/images/icon/ic_arrow_right.svg" alt="오른쪽 화살표" />
         </Link>
