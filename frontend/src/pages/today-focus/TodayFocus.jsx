62d32c6e40a488c5b48490e57cefa5c9427575df
Decal <vumublne@naver.com>
2025-04-02 15:10:57 +0900

    refactor: 전체적인 리팩터링, 버튼 변수 이름변경

--- frontend/src/pages/today-focus/TodayFocus.jsx
+++ frontend/src/pages/today-focus/TodayFocus.jsx
@@ -9,7 +9,7 @@ import StudyNavbar from "@components/study-navbar/StudyNavbar.jsx";
 
 const TodayFocus = () => {
   const { id } = useParams();
-  const [point, setPoint] = useState(0);
+  const [point, setPoint] = useState(5);
   const [totalPoint, setTotalPoint] = useState(0);
   const [start, setStart] = useState(false);
   const [pause, setPause] = useState(false);
@@ -47,8 +47,8 @@ const TodayFocus = () => {
 
   // 집중 성공 시 DB에 포인트 저장
   useEffect(() => {
-    if (start) return;
     if (point === 0) return;
+
     if (complete) {
       pointUpdate(id, { totalPoint });
     }
@@ -57,36 +57,36 @@ const TodayFocus = () => {
   // 집중 성공 시 총합 포인트 변경
   useEffect(() => {
     if (start) return;
-    if (!start)
-      if (complete) {
-        setTotalPoint((prevTotalPoint) => (prevTotalPoint += point));
-      }
+
+    if (!start && complete) {
+      setTotalPoint((prevTotalPoint) => (prevTotalPoint += point));
+    }
   }, [start, point]);
 
   // 시간 설정에 따른 포인트 설정
   const rewardPointSetByTime = (e) => {
-    if (isNaN(e.target.value)) return;
-    if (e.target.value < 10) {
+    const time = e.target.value;
+
+    if (isNaN(time)) return;
+    if (time < 10) {
       setPoint(0);
     } else {
-      setPoint(
-        e.target.value <= 19 ? 3 : 3 + Math.floor((e.target.value - 10) / 10)
-      );
+      setPoint(time <= 19 ? 3 : 3 + Math.floor(time / 10 - 1));
     }
   };
 
   // toast 팝업 자동종료
   useEffect(() => {
     const toastOff = setTimeout(() => {
-      if (!pause) {
-        if (!complete) {
-          return;
-        } else setComplete(false);
-      } else setPause(false);
+      if (!pause && !complete) return;
+
+      if (pause) setPause(false);
+      if (complete) setComplete(false);
     }, 2000);
 
     return () => {
-      if (complete) return;
+      if (pause || complete) return;
+
       clearTimeout(toastOff);
     };
   }, [pause, complete]);
@@ -101,7 +101,7 @@ const TodayFocus = () => {
         />
         <TodayFocusPoint totalPoint={totalPoint} />
         <div className={styles.focusContainer}>
-          <div className={styles.focus}>
+          <section className={styles.focus}>
             <h2 className={styles.focusTxt}>오늘의 집중</h2>
             <TodayFocusTimer
               rewardPointSetByTime={rewardPointSetByTime}
@@ -110,7 +110,7 @@ const TodayFocus = () => {
               start={start}
               setStart={setStart}
             />
-          </div>
+          </section>
         </div>
       </div>
       <TodayFocusToast pause={pause} complete={complete} point={point} />
