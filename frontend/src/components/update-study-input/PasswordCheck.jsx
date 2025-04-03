06234c2a5c6862414e70c70f59ba5b02ae5bbbf4
rakaso598 <nexonaddress98@gmail.com>
2025-04-03 16:48:04 +0900

    feat: 비밀번호 눈모양토글 확인란에도 추가

--- frontend/src/components/update-study-input/PasswordCheck.jsx
+++ frontend/src/components/update-study-input/PasswordCheck.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Input.module.css";
+import btnCloseIcon from "/images/icon/btn_visibility_on_24px-1.svg";
+import btnSeeIcon from "/images/icon/btn_visibility_on_24px.svg";
 
 const PasswordCheck = ({ password, onPasswordCheck }) => {
   const [passwordCheck, setPasswordCheck] = useState("");
   const [isActive, setIsActive] = useState(false);
+  const [seeCheck, setSeeCheck] = useState(false); // 비밀번호 확인 토글 상태
 
   useEffect(() => {
     if (passwordCheck === "") {
@@ -16,15 +19,29 @@ const PasswordCheck = ({ password, onPasswordCheck }) => {
     }
   }, [password, passwordCheck, onPasswordCheck]);
 
+  const handleCheckClick = () => {
+    setSeeCheck(!seeCheck);
+  };
+
   return (
     <label className={styles.label}>
       <p className={styles.inputBoxTitle}>비밀번호 확인</p>
-      <input
-        onChange={(e) => setPasswordCheck(e.target.value)}
-        className={`${styles.input} ${isActive ? styles.err : ""}`}
-        type="password"
-        placeholder="비밀번호를 다시 한 번 입력해주세요"
-      />
+      <div className={styles.pwInputBox}> {/* 비밀번호 input과 아이콘을 묶는 div */}
+        <input
+          onChange={(e) => setPasswordCheck(e.target.value)}
+          className={`${styles.input} ${isActive ? styles.err : ""}`}
+          type={seeCheck ? "text" : "password"}
+          placeholder="비밀번호를 다시 한 번 입력해주세요"
+          
+        />
+        <img
+          onClick={handleCheckClick}
+          className={styles.btnIcon}
+          src={seeCheck ? btnSeeIcon : btnCloseIcon}
+          alt="btnSeeIcon"
+        />
+      </div>
+
       {isActive && <p className={styles.errMessage}>*비밀번호가 일치하지 않습니다.</p>}
       {!isActive && passwordCheck !== "" && (
         <p className={styles.successMessage}>비밀번호가 일치합니다.</p>
