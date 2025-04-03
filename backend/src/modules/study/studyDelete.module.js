81008213effc8ccedda2bcc090680fea3ac56ed5
Jam1eL1 <23jisu@gmail.com>
2025-04-03 12:55:27 +0900

    refactor: studyDelete 모듈 주석 제거

--- backend/src/modules/study/studyDelete.module.js
+++ backend/src/modules/study/studyDelete.module.js
@@ -18,10 +18,9 @@ studyDeleteRouter.delete("/:studyId", async (req, res, next) => {
       return res.status(404).json({ message: "스터디를 찾을 수 없습니다." });
     }
 
-    // bcrypt로 평문 비밀번호와 저장된, 이미 암호화된 비밀번호 비교
     const passwordMatch = await bcrypt.compare(
-      password, // 사용자가 입력한 평문 비밀번호
-      study.encryptedPassword // DB에 저장된 암호화된 비밀번호
+      password,
+      study.encryptedPassword
     );
 
     if (!passwordMatch) {
@@ -31,7 +30,6 @@ studyDeleteRouter.delete("/:studyId", async (req, res, next) => {
       });
     }
 
-    // 비밀번호 확인 되었으므로 삭제 실행
     await prisma.study.delete({
       where: {
         id: parseInt(studyId),
