c59e1935d63a36510d0da1b5e8f9f3402dbbb690
Jam1eL1 <23jisu@gmail.com>
2025-04-07 09:48:25 +0900

    feat: verifyPassword api 추가

---
+++ frontend/src/api/study/verifyPassword.api.js
@@ -0,0 +1,21 @@
+import axiosInstance from "@api/axiosInstance";
+
+export const verifyStudyPassword = async (studyId, password) => {
+  try {
+    const response = await axiosInstance.post(
+      `/api/study/${studyId}/verify-password`,
+      {
+        encryptedPassword: password,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("비밀번호 확인 중 오류 발생:", error);
+    return {
+      success: false,
+      message:
+        error.response?.data?.message ||
+        "비밀번호 확인 중 오류가 발생했습니다.",
+    };
+  }
+};
