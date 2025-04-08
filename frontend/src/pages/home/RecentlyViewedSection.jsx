6144bdd0e569d6a88cdd5cba530cfe375ebedf68
gksktl111 <gksktl111@naver.com>
2025-04-08 14:00:09 +0900

    refactor : 강사님 코드리뷰 수정사항 반영

--- frontend/src/pages/home/RecentlyViewedSection.jsx
+++ frontend/src/pages/home/RecentlyViewedSection.jsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from "react";
-import HomeCard from "@components/home/HomeCard";
-import styles from "./RecentlySection.module.css";
-import { getRecentlyViewedStudies } from "@api/home/getStudy.api";
+import styles from "./RecentlyViewedSection.module.css";
 import { ClipLoader } from "react-spinners";
+import { getRecentlyViewedStudies } from "@api/home/home.api";
+import HomeCard from "./HomeCard";
 
-// 로컬 스토리지에 데이터가 있는지 확인
-const HAS_CARD_ID = localStorage.getItem("studyForestCardIds");
+const RecentlyVeiwedSection = () => {
+  const cardIds = localStorage.getItem("studyForestCardIds");
 
-const RecentlySection = () => {
   const [recentlyViewedStudies, setRecentlyViewedStudies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const data = localStorage.getItem("studyForestCardIds");
-
-    // 여기서 서버에서 카드 아이디에 따른 카드 데이터 조회하기 조회
+    // cardIds가 없다면 조회 X, 있다면 스터디 조회
     const fetchRecentlyViewedStudies = async () => {
-      if (!data) {
+      if (!cardIds) {
         return;
       }
 
       try {
         setIsLoading(true);
-        const recentlyViewedStudies = await getRecentlyViewedStudies(data);
+
+        // 최근 조회한 스터디 목록 조회
+        const recentlyViewedStudies = await getRecentlyViewedStudies(cardIds);
         setRecentlyViewedStudies(recentlyViewedStudies);
       } catch (error) {
         console.error("최근 조회한 스터디 가져오기 실패:", error);
@@ -38,7 +37,7 @@ const RecentlySection = () => {
     <div className={styles.recentStudyContainer}>
       <div className={styles.title}>최근 조회한 스터디</div>
 
-      {HAS_CARD_ID ? (
+      {cardIds ? (
         isLoading ? (
           <div className={styles.loading}>
             <ClipLoader size={100} color="#578246" loading={true} />
@@ -65,4 +64,4 @@ const RecentlySection = () => {
   );
 };
 
-export default RecentlySection;
+export default RecentlyVeiwedSection;
